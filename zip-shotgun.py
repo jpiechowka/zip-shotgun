04b413975494a5eb4a911b8136e4462ae1d29290
jpiechowka <januszpiechowka@gmail.com>
2018-11-14 00:00:36 +0100

    Add initial application skeleton 2

--- zip-shotgun.py
+++ zip-shotgun.py
@@ -1,10 +1,18 @@
 import click
 
+from utils.logging_config import configure_logging
 
-@click.command
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+APP_NAME = 'ZIP Shotgun'
+VERSION = '1.0.0'
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version=VERSION, prog_name=APP_NAME)
 def zip_shotgun():
     pass
 
 
 if __name__ == '__main__':
+    configure_logging()
     zip_shotgun()
